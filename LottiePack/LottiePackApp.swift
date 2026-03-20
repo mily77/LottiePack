c097384ef4f583c91fb977a4bc7a758772076da8
Emily <773258709@qq.com>
2026-03-20 11:25:58 +0800

    Initial Commit

---
+++ LottiePack/LottiePackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottiePackApp.swift
+//  LottiePack
+//
+//  Created by Emily Huang on 2026/3/20.
+//
+
+import SwiftUI
+
+@main
+struct LottiePackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LottiePackTests/LottiePackTests.swift
@@ -0,0 +1,17 @@
+//
+//  LottiePackTests.swift
+//  LottiePackTests
+//
+//  Created by Emily Huang on 2026/3/20.
+//
+
+import Testing
+@testable import LottiePack
+
+struct LottiePackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
